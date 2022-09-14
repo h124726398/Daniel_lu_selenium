231138a29f28fb4b9de1e9777e9b0395318e8ca6
h124726398 <h124726398@gmail.com>
2022-09-14 23:13:59 +0800

    version2

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -23,6 +24,14 @@ login = 1
 driver.get("http://10.20.1.21:8080/business-central")
 driver.maximize_window()
 
+
+def is_element_present(by, value):
+    try:
+        driver.find_element(by, value)
+    except:
+        return False
+    return True
+
 while(login):
     try:
         inputname = driver.find_element(By.XPATH, "//*[@id=\"login-content\"]/form/fieldset/input[1]")
@@ -44,8 +53,8 @@ while(login):
         space_name = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div[2]/div/div[3]/div/div[11]/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div[2]/div/div/div/div[1]/div/div/div[2]/div[1]/input")))
         # space_name = driver.find_element(By.XPATH, "/html/body/div[4]/div[2]/div/div[3]/div/div[11]/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div[2]/div/div/div/div[1]/div/div/div[2]/div[1]/input")
         space_description = driver.find_element(By.XPATH, "/html/body/div[4]/div[2]/div/div[3]/div/div[11]/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div[2]/div/div/div/div[1]/div/div/div[2]/div[2]/input")
-        space_name_input = "test2"
-        space_description_input = "test2"
+        space_name_input = "test15"
+        space_description_input = "test15"
         space_name.send_keys(space_name_input)
         space_description.send_keys(space_description_input)
         time.sleep(5)
@@ -55,8 +64,6 @@ while(login):
         # click new space
         title = WebDriverWait(driver,5).until(EC.visibility_of_all_elements_located((By.XPATH,"//h2[contains(@class,'card-pf-title')]")))
         for number, data in enumerate(title, start=1):
-            print("nu", number)
-            print("data", data.text)
             if data.text == space_name_input:
                 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div[2]/div/div[3]/div/div[11]/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div[2]/div/div/div/div/div/div[2]/div/div[%s]/div" % number))).click()
                 break
@@ -75,20 +82,34 @@ while(login):
         # click ok buttom
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[4]/div[2]/div/div[3]/div/div[11]/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div[2]/div/div/div/div/div[1]/div/div[2]/button[2]"))).click()
         # click deploy_button
-        time.sleep(3)
+        time.sleep(5)
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, 'deploy'))).click()
-
-        # create action chain object
-        # element = driver.find_element(By.ID, ("deploy"));
-        # actions = ActionChains(driver)
-        # actions.moveToElement(element).click().build().perform();
-        print("outside")
         time.sleep(3)
-        print(driver.current_url)
+        while(True):
+            # 法1 有bug
+            # state_exist2 = is_element_present(By.XPATH, "/html/body/div[7]/div/table/tbody/tr[2]/td[2]/div/div")
+            # time.sleep(3)
+            # if state_exist2 == True:
+            #     print("inside")
+            #     data = WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element((By.XPATH, "/html/body/div[7]/div/table/tbody/tr[2]/td[2]/div/div"), 'Deploy to server configuration successful and container successfully updated.'))
+            #     print("data", data)
+            #     time.sleep(3)
+            #     if data == True:
+            #         break
+            # 法2
+            # state_exist2 = is_element_present(By.XPATH, "/html/body/div[4]/div[2]/div/div[3]/div/div[11]/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div[2]/div/div/div/div/div/div/div/div/div[3]/div/div[2]/div/div/table/tbody")
+            data = driver.find_element(By.XPATH, "/html/body/div[4]/div[2]/div/div[3]/div/div[11]/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div[2]/div/div/div/div/div/div/div/div/div[3]/div/div[2]/div/div/table/tbody").text
+        # for i in data:
+            print("data", data)
+            time.sleep(10)
+            # data = WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element((By.XPATH, "/html/body/div[4]/div[2]/div/div[3]/div/div[11]/div/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div[2]/div/div/div[2]/div/div/div/div/div/div/div/div/div[3]/div/div[2]/div/div/table/tbody"), "Build of module 'kafka-drools-test' (requested by admin) completed.Build: SUCCESSFUL"))
+            if "Build of module 'kafka-drools-test' (requested by admin) completed. Build: SUCCESSFUL" in data:
+                print("inside")
+                time.sleep(3)
+                print(driver.current_url)
+                driver.close()
+                break
     except Exception as e:
         print("Exception", e)
         print(driver.current_url)
-driver.close()
 
-# def delete_space():
-    # heading = driver.find_element_by_tag_name('h5')
